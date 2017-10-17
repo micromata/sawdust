aa100cf3db0f3ba35df9e0858a53f6fc3552e50f
Christian Stein <sormuras@gmail.com>
2017-10-17 11:43:20 +0200

    Install Apache Ant 1.10.1 and use it

---
+++ scripts/install-ant.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+ANT_VERSION=1.10.1
+ANT_ARCHIVE=apache-ant-${ANT_VERSION}-bin.tar.gz
+
+cd ~
+wget http://www.us.apache.org/dist/ant/binaries/${ANT_ARCHIVE}
+tar -xzf ${ANT_ARCHIVE}
+export ANT_HOME=~/apache-ant-${ANT_VERSION}
+export PATH=${ANT_HOME}/bin:$PATH
+cd -
