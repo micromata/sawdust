73ef6748563686cbfe1708c714bac6fef349aeca
Christian Stein <sormuras@gmail.com>
2018-06-26 00:22:58 +0200

    Update Maven build files
    - JUnit 5.3.0-M1
    - SLF4J 1.8.0-beta2
    - maven-compiler-plugin 3.7.1-SNAPSHOT
    - maven-surefire-plugin 2.22.0

--- modules/user.view/src/test/java/user.view/user/view/UserViewTests.java
+++ modules/user.view/src/test/java/user.view/user/view/UserViewTests.java
@@ -1,9 +1,12 @@
 package user.view;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import sawdust.alpha.AlphaPublic;
 
 @DisplayName("[user.view] UserViewTests")
@@ -11,5 +14,14 @@ class UserViewTests {
   @Test
   void accessExportedTypes() {
     assertEquals("AlphaPublic", AlphaPublic.class.getSimpleName());
+  }
+
+  @Test
+  @EnabledIfSystemProperty(named = "modular.tests", matches = "true")
+  void modular() {
+    var module = UserViewTests.class.getModule();
+    assertEquals("user.view", module.getName());
+    assertFalse(module.getDescriptor().isAutomatic());
+    assertTrue(module.getDescriptor().isOpen());
   }
 }
