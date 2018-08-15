f82cd57b2a45c9f8921d97247ec5428840546149
Christian Stein <sormuras@gmail.com>
2018-08-15 10:11:08 +0200

    Fix base url for Apache Ant downloads

--- scripts/install-ant.sh
+++ scripts/install-ant.sh
@@ -5,7 +5,7 @@ ANT_VERSION=1.10.4
 ANT_ARCHIVE=apache-ant-${ANT_VERSION}-bin.tar.gz
 
 cd ~
-wget http://www.us.apache.org/dist/ant/binaries/${ANT_ARCHIVE}
+wget https://archive.apache.org/dist/ant/binaries/${ANT_ARCHIVE}
 tar -xzf ${ANT_ARCHIVE}
 export ANT_HOME=~/apache-ant-${ANT_VERSION}
 export PATH=${ANT_HOME}/bin:$PATH
