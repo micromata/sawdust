0e854acacd8258feaae77708aeca2c895fe6a0c9
Christian Stein <sormuras@gmail.com>
2018-09-10 07:25:44 +0200

    Add mode-2-modular-patch-runtime-whitebox project

---
+++ mode-2-modular-patch-runtime-whitebox/src/test/java/foo/PublicFooTests.java
@@ -0,0 +1,11 @@
+package foo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PublicFooTests {
+  @Test
+  void accessPublicFoo() {
+    Assertions.assertEquals("foo", PublicFoo.class.getPackageName());
+  }
+}
